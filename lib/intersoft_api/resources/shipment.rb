cfbdb71a7ee842355cccb871710aad3d9d739b7d
nickfarm27 <nicholasfarm27@gmail.com>
2023-10-31 12:57:21 +0800

    Create fetch shipments function

--- lib/intersoft_api/resources/shipment.rb
+++ lib/intersoft_api/resources/shipment.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module IntersoftAPI
   class ShipmentResource < Resource
     def create(attributes)
@@ -8,6 +6,15 @@ module IntersoftAPI
 
     def generate_label_qr_code(shipment_id)
       Shipment.new(get_request("shipments/printMyLabelQRCode/rm/#{shipment_id}").body)
+    end
+
+    def all(shipping_location_id, options = {})
+      url = "shipments/#{shipping_location_id}"
+      options = options.slice(:shipping_account_id, :carrier_code, :status, :destination_country_code, :date_from, :date_to, :page_size, :page_number)
+      options = options.transform_keys { |key| key.to_s.camelize(:lower) }
+      url += "?#{options.to_query}" if options.any?
+
+      Shipment.new(get_request(url).body)
     end
   end
 end
